c020829a4c7e56b130c4c6a6abe598e4be22eba3
Phablo Vilas Boas <phablo_sena@hotmail.com>
2022-06-10 05:35:32 -0300

    fix: new bugfix

--- release.config.js
+++ release.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    branchs: "master",
-    repositoryurl: "https://github.com/phablo200/react-app-cicd",
-    plugins: [
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator', 
-        [
-            '@semantic-release/github', {
-               assets: [
-                   { path: 'build.zip', label: 'Build' },
-                   { path: 'coverage.zip', label: 'Coverage' }
-               ] 
-            }
-        ]
-    ]
+  branchs: "master",
+  repositoryurl: "https://github.com/phablo200/react-app-cicd",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
 };
