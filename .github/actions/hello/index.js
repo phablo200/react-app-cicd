bcdc081324ebac618db21f59dd43930401e8db44
Phablo Vilas Boas <phablo_sena@hotmail.com>
2022-06-15 05:40:14 -0300

    feat: teste
    teste

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,14 +3,22 @@ const github = require("@actions/github");
 
 try {
   // throw new Error('Some error message')
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.weeoe('Error message');
 
   const name = core.getInput("who-to-greet");
+  core.setSecret('some string here');
   console.log(`Hello World ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup('Loggin github object');
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable('HELLO', 'hello');
 } catch (error) {
   core.setFailed(error.message);
 }
